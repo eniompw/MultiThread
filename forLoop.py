1fd1379f4099a564329aa322fbc6399c2b25b4ab
eniompw <71254710+eniompw@users.noreply.github.com>
2020-11-11 09:03:26 +0000

    Create forLoop.py

---
+++ forLoop.py
@@ -0,0 +1,21 @@
+import requests
+import re
+
+f = open("constituents.csv",'r' )
+csv = f.readlines()
+f. close()
+sp500 = []
+
+for x in range(1,len(csv)):
+    csvline = csv[x].split(",")
+    company = csvline[0]
+    try:
+        req = requests.get('https://www.marketbeat.com/stocks/'+company,timeout=10)
+        pos = req.text.find("Dividend Yield<strong>")
+        co = []
+        co.append(company)
+        co.append(req.text[pos+22:pos+26])
+        print(co)
+        sp500.append(co)
+    except Exception as e:
+        print(e)
